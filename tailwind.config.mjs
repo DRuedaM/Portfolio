c369600f2d325fadbb11e757fc0d30b89bd41eb3
DRuedaM <davidruedamartin26@gmail.com>
2024-05-28 20:53:25 +0200

    Nuevas tarjetas de proyectos personales, tamaño de titulos aumentado, mas margen despues del titulo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+		},
 	},
-	plugins: [],
+	plugins: [
+    ]
 }
 
 
